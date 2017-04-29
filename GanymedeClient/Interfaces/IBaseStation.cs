57e0e823a2486bacce164f7d6baaa3fb5318b3bc
Jason Folda <jfolda@gmail.com>
2017-04-29 16:15:39 -0700

    Adding Interfaces for use between Communication Project, and UI.

---
+++ GanymedeClient/Interfaces/IBaseStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ganymede.Interfaces
+{
+    public interface IBaseStation
+    {
+        #region Read Data
+
+        /// <summary>
+        /// The unique identifier for this base station.
+        /// </summary>
+        string Id { get; }
+
+        /// <summary>
+        /// The flow rate of water to the Pods. This is the main line, so total flow.
+        /// </summary>
+        double FlowRate { get; }
+
+        /// <summary>
+        /// The pods that are connected to this specific base.
+        /// </summary>
+        IEnumerable<IPod> PodsConnectedToBase { get; }
+
+        #endregion
+    }
+}
